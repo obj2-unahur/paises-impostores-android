ded92b89ca5fc217bbbe720c0ff6e0a53c28b410
Rodrigo Alfonso <rodri042@gmail.com>
2021-07-28 07:27:52 -0300

    Formateo

--- app/src/test/java/ar/edu/unahur/obj2/claseBonusKotlinAndroid/PaisesImpostoresTest.kt
+++ app/src/test/java/ar/edu/unahur/obj2/claseBonusKotlinAndroid/PaisesImpostoresTest.kt
@@ -14,29 +14,79 @@ import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 
-class PaisesImpostoresTest:DescribeSpec({
+class PaisesImpostoresTest : DescribeSpec({
     val api = mockk<RestCountriesAPI>()
     Observatorio.api = api
 
     // Etapa 1
     // 1.1.1
     describe("Un pais") {
-
-        val estadosUnidos = Pais("Estados Unidos", "EEUU", 525000000, 400000.0, "America", listOf("NORMER"), listOf("Ingles"),
-            mutableListOf())
-        val canada = Pais("Canada", "Can", 500000000, 400000.0, "America", listOf("NORMER"), listOf("Ingles", "Frances"),
-            mutableListOf(estadosUnidos))
-        val francia = Pais("Francia", "FRA", 510000000, 400000.0, "Europa", listOf("EUROMER"), listOf("Francés"),mutableListOf())
-        val argentina = Pais("Argentina", "ARG", 440000000, 400000.0, "America", listOf("Mercosur"), listOf("español"),mutableListOf())
-        val mexico = Pais("Mexico", "MEX", 510000000, 400000.0, "America", listOf("NORMER"), listOf("español"),mutableListOf(canada,estadosUnidos))
-        val australia = Pais("Australia", "AUS", 510000000, 400000.0, "Oceania", listOf("OCEAMER"), listOf("Ingles"),mutableListOf())
+        val estadosUnidos = Pais(
+            "Estados Unidos",
+            "EEUU",
+            525000000,
+            400000.0,
+            "America",
+            listOf("NORMER"),
+            listOf("Ingles"),
+            mutableListOf()
+        )
+        val canada = Pais(
+            "Canada",
+            "Can",
+            500000000,
+            400000.0,
+            "America",
+            listOf("NORMER"),
+            listOf("Ingles", "Frances"),
+            mutableListOf(estadosUnidos)
+        )
+        val francia = Pais(
+            "Francia",
+            "FRA",
+            510000000,
+            400000.0,
+            "Europa",
+            listOf("EUROMER"),
+            listOf("Francés"),
+            mutableListOf()
+        )
+        val argentina = Pais(
+            "Argentina",
+            "ARG",
+            440000000,
+            400000.0,
+            "America",
+            listOf("Mercosur"),
+            listOf("español"),
+            mutableListOf()
+        )
+        val mexico = Pais(
+            "Mexico",
+            "MEX",
+            510000000,
+            400000.0,
+            "America",
+            listOf("NORMER"),
+            listOf("español"),
+            mutableListOf(canada, estadosUnidos)
+        )
+        val australia = Pais(
+            "Australia",
+            "AUS",
+            510000000,
+            400000.0,
+            "Oceania",
+            listOf("OCEAMER"),
+            listOf("Ingles"),
+            mutableListOf()
+        )
         estadosUnidos.paisesLimitrofes.add(mexico)
         estadosUnidos.paisesLimitrofes.add(canada)
         canada.paisesLimitrofes.add(mexico)
 
         it("con mas de 1 idioma oficial es Plurinacional") {
             canada.esPlurinacional().shouldBeTrue()
-
         }
         it("con 1 idioma oficial NO es Plurinacional") {
             estadosUnidos.esPlurinacional().shouldBeFalse()
@@ -61,7 +111,6 @@ class PaisesImpostoresTest:DescribeSpec({
         // 1.1.4
         describe("conoce al vecino mas poblado") {
             it("siendo EEUU para el caso de Mexico") {
-
                 mexico.vecinoMasPoblado().shouldBe(estadosUnidos)
             }
             it("siendo EEUU para el caso de EEUU") {
@@ -73,7 +122,6 @@ class PaisesImpostoresTest:DescribeSpec({
         // 1.2.1
         describe("es limítrofe con otro país") {
             it("Mexico es limitrofe con canadá") {
-
                 mexico.esLimitrofeCon(canada).shouldBeTrue()
             }
             it("EEUU NO es limitrofe con Francia") {
@@ -139,14 +187,17 @@ class PaisesImpostoresTest:DescribeSpec({
                     listOf("ARG"), emptyList(), emptyList()
                 )
             )
-            every {api.buscarPaisesPorNombre("Francia")} returns listOf(Country(
-                "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
-                listOf(), emptyList(), emptyList()
-            ))
-            every { api.todosLosPaises() } returns listOf(Country(
-                "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
-                listOf("PAR"), emptyList(), emptyList()
-            ),
+            every { api.buscarPaisesPorNombre("Francia") } returns listOf(
+                Country(
+                    "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
+                    listOf(), emptyList(), emptyList()
+                )
+            )
+            every { api.todosLosPaises() } returns listOf(
+                Country(
+                    "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
+                    listOf("PAR"), emptyList(), emptyList()
+                ),
                 Country(
                     "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
                     listOf("ARG"), emptyList(), emptyList()
@@ -154,7 +205,8 @@ class PaisesImpostoresTest:DescribeSpec({
                 Country(
                     "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
                     listOf(), emptyList(), emptyList()
-                ))
+                )
+            )
             every { api.paisConCodigo("PAR") } returns Country(
                 "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
                 listOf("ARG"), emptyList(), emptyList()
@@ -163,13 +215,13 @@ class PaisesImpostoresTest:DescribeSpec({
                 "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
                 listOf(), emptyList(), emptyList()
             )
-            every {api.paisConCodigo("ARG")} returns Country(
+            every { api.paisConCodigo("ARG") } returns Country(
                 "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
                 listOf("PAR"), emptyList(), emptyList()
             )
 
             it("Argentina y paraguay son limitrofes") {
-                Observatorio.sonLimitrofes("Argentina","Paraguay").shouldBeTrue()
+                Observatorio.sonLimitrofes("Argentina", "Paraguay").shouldBeTrue()
             }
 
             it("Argentina y Francia NO son limitrofes") {
@@ -187,10 +239,12 @@ class PaisesImpostoresTest:DescribeSpec({
                 )
 
             )
-            every {api.buscarPaisesPorNombre("Francia")} returns listOf(Country(
-                "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
-                listOf(), emptyList(), emptyList()
-            ))
+            every { api.buscarPaisesPorNombre("Francia") } returns listOf(
+                Country(
+                    "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
+                    listOf(), emptyList(), emptyList()
+                )
+            )
             every { api.paisConCodigo("PAR") } returns Country(
                 "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
                 listOf("ARG"), emptyList(), emptyList()
@@ -198,19 +252,19 @@ class PaisesImpostoresTest:DescribeSpec({
             every { api.buscarPaisesPorNombre("Paraguay") } returns listOf(
                 Country(
                     "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
-                    listOf("ARG"),listOf(Language("Guarani"),Language("Español")), emptyList()
+                    listOf("ARG"), listOf(Language("Guarani"), Language("Español")), emptyList()
                 )
             )
-            every {api.paisConCodigo("ARG")} returns Country(
+            every { api.paisConCodigo("ARG") } returns Country(
                 "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
                 listOf("PAR"), emptyList(), emptyList()
             )
 
-            it("Argentina y Francia necesitan traduccion."){
-                Observatorio.necesitanTraduccion("Argentina","Francia").shouldBeTrue()
+            it("Argentina y Francia necesitan traduccion.") {
+                Observatorio.necesitanTraduccion("Argentina", "Francia").shouldBeTrue()
             }
-            it("Argentina y Paraguay no necesitan traduccion."){
-                Observatorio.necesitanTraduccion("Argentina","Paraguay").shouldBeFalse()
+            it("Argentina y Paraguay no necesitan traduccion.") {
+                Observatorio.necesitanTraduccion("Argentina", "Paraguay").shouldBeFalse()
             }
         }
 
@@ -218,34 +272,50 @@ class PaisesImpostoresTest:DescribeSpec({
         describe("saber si 2 paises son potenciales aliados") {
             every { api.buscarPaisesPorNombre("Argentina") } returns listOf(
                 Country(
-                    "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
-                    listOf("PAR"), listOf(Language("Español")), listOf(RegionalBloc("Mercosur","MER"))
+                    "Argentina",
+                    "ARG",
+                    "Buenos Aires",
+                    "America",
+                    40000000,
+                    100000.0,
+                    listOf("PAR"),
+                    listOf(Language("Español")),
+                    listOf(RegionalBloc("Mercosur", "MER"))
                 )
 
             )
-            every {api.buscarPaisesPorNombre("Francia")} returns listOf(Country(
-                "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
-                listOf(), emptyList(), listOf(RegionalBloc("Mercosur","MER"))
-            ))
+            every { api.buscarPaisesPorNombre("Francia") } returns listOf(
+                Country(
+                    "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
+                    listOf(), emptyList(), listOf(RegionalBloc("Mercosur", "MER"))
+                )
+            )
             every { api.paisConCodigo("PAR") } returns Country(
                 "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
-                listOf("ARG"), emptyList(), listOf(RegionalBloc("Mercosur","MER"))
+                listOf("ARG"), emptyList(), listOf(RegionalBloc("Mercosur", "MER"))
             )
             every { api.buscarPaisesPorNombre("Paraguay") } returns listOf(
                 Country(
-                    "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
-                    listOf("ARG"),listOf(Language("Guarani"),Language("Español")),listOf(RegionalBloc("Mercosur","MER"))
+                    "Paraguay",
+                    "PAR",
+                    "Asuncion",
+                    "America",
+                    40000000,
+                    100000.0,
+                    listOf("ARG"),
+                    listOf(Language("Guarani"), Language("Español")),
+                    listOf(RegionalBloc("Mercosur", "MER"))
                 )
             )
-            every {api.paisConCodigo("ARG")} returns Country(
+            every { api.paisConCodigo("ARG") } returns Country(
                 "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
-                listOf("PAR"), emptyList(), listOf(RegionalBloc("Mercosur","MER"))
+                listOf("PAR"), emptyList(), listOf(RegionalBloc("Mercosur", "MER"))
             )
-            it("Argentina y paraguay son potenciales aliados."){
-                Observatorio.sonPotencialesAliados("Argentina","Paraguay").shouldBeTrue()
+            it("Argentina y paraguay son potenciales aliados.") {
+                Observatorio.sonPotencialesAliados("Argentina", "Paraguay").shouldBeTrue()
             }
-            it("Argentina y mexico no son potenciales aliados."){
-                Observatorio.sonPotencialesAliados("Argentina","Francia").shouldBeFalse()
+            it("Argentina y mexico no son potenciales aliados.") {
+                Observatorio.sonPotencialesAliados("Argentina", "Francia").shouldBeFalse()
             }
         }
 
@@ -264,19 +334,49 @@ class PaisesImpostoresTest:DescribeSpec({
                     "Francia", "FRA", "Paris", "Europa", 40000000, 100000.0,
                     listOf(), emptyList(), emptyList()
                 ),
-                Country("España", "ESP", "Madrid", "Europa", 100, 100.1, emptyList(), emptyList(), emptyList()),
-                Country("Groenlandia", "GRO", "Nose", "Europa", 114, 100.1, emptyList(), emptyList(), emptyList()),
-                Country("Italia", "ITA", "Roma", "Europa", 1000000, 100.1, emptyList(), emptyList(), emptyList()),
+                Country(
+                    "España",
+                    "ESP",
+                    "Madrid",
+                    "Europa",
+                    100,
+                    100.1,
+                    emptyList(),
+                    emptyList(),
+                    emptyList()
+                ),
+                Country(
+                    "Groenlandia",
+                    "GRO",
+                    "Nose",
+                    "Europa",
+                    114,
+                    100.1,
+                    emptyList(),
+                    emptyList(),
+                    emptyList()
+                ),
+                Country(
+                    "Italia",
+                    "ITA",
+                    "Roma",
+                    "Europa",
+                    1000000,
+                    100.1,
+                    emptyList(),
+                    emptyList(),
+                    emptyList()
+                ),
             )
             it("obtener los codigos ISO de los 5 paises con mayor densidad poblacional") {
-
-                Observatorio.codigoIso5PaisesMasDensos().shouldContainExactly("ITA", "ARG", "PAR", "FRA", "GRO")
+                Observatorio.codigoIso5PaisesMasDensos()
+                    .shouldContainExactly("ITA", "ARG", "PAR", "FRA", "GRO")
             }
-            it("indicar el nombre del continente con mas países plurinacionales"){ //2.5
+            it("indicar el nombre del continente con mas países plurinacionales") { //2.5
                 every { api.buscarPaisesPorNombre("Paraguay") } returns listOf(
                     Country(
                         "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
-                        listOf("ARG"),listOf(Language("Guarani"),Language("Español")), emptyList()
+                        listOf("ARG"), listOf(Language("Guarani"), Language("Español")), emptyList()
                     )
                 )
                 Observatorio.continenteConMasPlurinacionales().shouldBe("America")
@@ -284,18 +384,21 @@ class PaisesImpostoresTest:DescribeSpec({
         }
 
         // 2.6
-        it("Promedio de densidad de paises isla siendo australia la unica isla."){
-            every {api.todosLosPaises()}returns listOf(Country(
-                "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
-                listOf("ARG"),listOf(Language("Guarani"),Language("Español")), emptyList()
-            ),
+        it("Promedio de densidad de paises isla siendo australia la unica isla.") {
+            every { api.todosLosPaises() } returns listOf(
+                Country(
+                    "Paraguay", "PAR", "Asuncion", "America", 40000000, 100000.0,
+                    listOf("ARG"), listOf(Language("Guarani"), Language("Español")), emptyList()
+                ),
                 Country(
                     "Australia", "AUS", "Auckland", "Oceania", 40000000, 100000.0,
-                    listOf(),listOf(Language("Ingles"),Language("Frances")), emptyList()
-                ))
-            every {api.paisConCodigo("ARG")} returns Country(
+                    listOf(), listOf(Language("Ingles"), Language("Frances")), emptyList()
+                )
+            )
+            every { api.paisConCodigo("ARG") } returns Country(
                 "Argentina", "ARG", "Buenos Aires", "America", 40000000, 100000.0,
-                listOf("PAR"), emptyList(), listOf(RegionalBloc("Mercosur","MER")))
+                listOf("PAR"), emptyList(), listOf(RegionalBloc("Mercosur", "MER"))
+            )
             Observatorio.promedioDeDensidadDePaisesIsla().shouldBe(400.0)
         }
     }
